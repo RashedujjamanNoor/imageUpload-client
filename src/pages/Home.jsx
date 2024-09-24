45a1bc2071aed4b75b3774ae09ad0162e85a058f
RashedujjamanNoor <noorrk042@gmail.com>
2024-09-24 22:51:51 +0600

    fix bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-
+import axios from "axios";
 const Home = () => {
   const [file, setFile] = useState();
   const image = new FormData();
   image.append("file", file);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    await axios
+      .post("http://localhost:5000/upload", image)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
